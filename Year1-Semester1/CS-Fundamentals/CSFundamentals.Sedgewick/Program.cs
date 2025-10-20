36ef01edbbd40430750657ae8488563f1bd87d5b
Abubakar Ahmed <bm.abubakar.1997@outlook.com>
2025-10-20 20:01:09 +0530

    Sedgweick's CS Fundamentals Chapter 1 Homework & Exercises

---
+++ Year1-Semester1/CS-Fundamentals/CSFundamentals.Sedgewick/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+using CSFundamentals.Sedgewick.Chapter1;
+
+if (args.Length == 0)
+{
+    System.Console.WriteLine("Give me an args");
+//    return;
+}
+
+var exercise = new Exercise2_28();
+exercise.Run(args);
