adecdc0800b59f9541a3047a482a668a734c5058
Abubakar Ahmed <bm.abubakar.1997@outlook.com>
2025-11-09 13:59:35 +0530

    Solved basic problems for section 3 in chapter 1.

---
+++ Year1-Semester1/CS-Fundamentals/CSFundamentals.Sedgewick/Chapter1/Exercise3_34.cs
@@ -0,0 +1,42 @@
+﻿namespace CSFundamentals.Sedgewick.Chapter1;
+
+public class Exercise3_34 : IExercise
+{
+    
+    //TODO: Bad solution. Refactor this solution
+    public void Run(string[] args)
+    {
+        var n = int.Parse(args[0]);
+        var cubeRoot = Math.Floor(Math.Cbrt(n));
+        for (int i = 0; i < cubeRoot; i++)
+        {
+            for (int j = 0; j < cubeRoot; j++)
+            {
+                var total = Math.Pow(i, 3) + Math.Pow(j, 3);
+                if (total == n)
+                {
+                    System.Console.WriteLine($"{i} - {j}");
+                }
+            }
+        }
+        
+
+        for(int i = 0; i < n; i++)
+        {
+            for (int j = 0; j< n; j++)
+            {
+                for (int k = 0; k < cubeRoot; k++)
+                {
+                    for (int b = 0; b < cubeRoot; b++)
+                    {
+                        var total = Math.Pow(i, 3) + Math.Pow(j, 3);
+                        if (total == n)
+                        {
+                            System.Console.WriteLine($"{i} - {j}");
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
