6a172ce13ae1b07705fd46817efd67dcf01481da
Abubakar Ahmed <bm.abubakar.1997@outlook.com>
2025-12-14 19:32:19 +0530

    Section 3 done

--- Year1-Semester1/CS-Fundamentals/CSFundamentals.Sedgewick/Program.cs
+++ Year1-Semester1/CS-Fundamentals/CSFundamentals.Sedgewick/Program.cs
@@ -1,10 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using CSFundamentals.Sedgewick.Chapter1;
+using CSFundamentals.Sedgewick.Chapter1.Section3;
 
 if (args.Length == 0)
 {
     System.Console.WriteLine("Give me an args");
 }
 
-IExercise exercise = new Exercise3_34();
+IExercise exercise = new Exercise3_42();
 exercise.Run(args);
