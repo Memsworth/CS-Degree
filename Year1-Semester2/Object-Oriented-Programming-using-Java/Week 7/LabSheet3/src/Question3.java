aec0f752568dd09729b72ef280bd9c916386031d
Abubakar Ahmed <bm.abubakar.1997@outlook.com>
2026-01-17 08:56:28 +0000

    Final implementation for q3 in labsheet 3

--- Year1-Semester2/Object-Oriented-Programming-using-Java/Week 7/LabSheet3/src/Question3.java
+++ Year1-Semester2/Object-Oriented-Programming-using-Java/Week 7/LabSheet3/src/Question3.java
@@ -1,6 +1,22 @@
 public class Question3 implements IExercise {
     public void Run() {
 
+        var emp = new Employee(1, "John", "Doe", 5000);
+
+        System.out.println("ID: " + emp.getID());
+        System.out.println("First Name: " + emp.getFirstName());
+        System.out.println("Last Name: " + emp.getLastName());
+        System.out.println("Full Name: " + emp.getName());
+        System.out.println("Monthly Salary: " + emp.getSalary());
+        System.out.println("Annual Salary: " + emp.getAnnualSalary());
+
+        emp.raiseSalary(10);
+        System.out.println("Salary after 10% raise: " + emp.getSalary());
+
+        emp.setSalary(6000);
+        System.out.println("Updated Salary: " + emp.getSalary());
+
+        System.out.println(emp);
     }
 
     public class Employee {
@@ -8,52 +24,52 @@ public class Question3 implements IExercise {
         private String FirstName;
         private String LastName;
         private int Salary;
-    }
 
-    public Employee(int id, String firstName, String lastName, int salary) {
-        Id = id;
-        FirstName = firstName;
-        LastName = lastName;
-        Salary = salary;
-    }
+        public Employee(int id, String firstName, String lastName, int salary) {
+            Id = id;
+            FirstName = firstName;
+            LastName = lastName;
+            Salary = salary;
+        }
 
-    public int getID() {
-        return Id;
-    }
+        public int getID() {
+            return Id;
+        }
 
-    public String getFirstName() {
-        return FirstName;
-    }
+        public String getFirstName() {
+            return FirstName;
+        }
 
-    public String getLastName() {
-        return LastName;
-    }
+        public String getLastName() {
+            return LastName;
+        }
 
-    public String getName() {
-        return FirstName + " " + LastName;
-    }
+        public String getName() {
+            return FirstName + " " + LastName;
+        }
 
-    public int getSalary() {
-        return Salary;
-    }
+        public int getSalary() {
+            return Salary;
+        }
 
-    public void setSalary(int salary) {
-        Salary = salary;
-    }
+        public void setSalary(int salary) {
+            Salary = salary;
+        }
 
-    public int getAnnualSalary() {
-        return Salary * 12;
-    }
+        public int getAnnualSalary() {
+            return Salary * 12;
+        }
 
-    public int raiseSalary(int percent) {
-        Salary += Salary * percent / 100;
-        return Salary;
-    }
+        public int raiseSalary(int percent) {
+            Salary += Salary * percent / 100;
+            return Salary;
+        }
 
-    @Override
-    public String toString() {
-        return "Employee[id=" + Id +
-               ", name=" + getName() +
-               ", salary=" + Salary + "]";
+        @Override
+        public String toString() {
+            return "Employee[id=" + Id +
+                    ", name=" + getName() +
+                    ", salary=" + Salary + "]";
+        }
     }
 }
