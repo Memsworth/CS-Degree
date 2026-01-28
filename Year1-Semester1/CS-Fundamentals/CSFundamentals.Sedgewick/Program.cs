9fe1a01d56e1dc788b052170bcb79a94c81f1d19
Abubakar Ahmed <bm.abubakar.1997@outlook.com>
2026-01-28 10:23:34 +0530

    chapter 4 tasks

--- Year1-Semester1/CS-Fundamentals/CSFundamentals.Sedgewick/Program.cs
+++ Year1-Semester1/CS-Fundamentals/CSFundamentals.Sedgewick/Program.cs
@@ -1,11 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using CSFundamentals.Sedgewick.Chapter1;
-using CSFundamentals.Sedgewick.Chapter1.Section3;
+using CSFundamentals.Sedgewick.Chapter1.Section4;
 
 if (args.Length == 0)
 {
     System.Console.WriteLine("Give me an args");
 }
 
-IExercise exercise = new Exercise3_42();
+IExercise exercise = new Exercise4_12();
 exercise.Run(args);
